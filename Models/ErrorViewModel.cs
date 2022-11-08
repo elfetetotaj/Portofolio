8c62d4bb38ae16bc955fb6b3bab947b473cb5841
elfetetotaj <elfetetotaj@hotmail.com>
2022-11-08 16:54:15 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portofolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
