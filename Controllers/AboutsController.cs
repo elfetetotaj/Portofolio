708c18aff30859a1cd8d470b6cb69e4db5f099c0
elfetetotaj <elfetetotaj@hotmail.com>
2022-11-09 21:12:17 +0100

    Navbar and AboutMe section

---
+++ Controllers/AboutsController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Portofolio.Data;
+using Portofolio.Models;
+
+namespace Portofolio.Controllers
+{
+    public class AboutsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AboutsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Abouts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Abouts.ToListAsync());
+        }
+
+        // GET: Abouts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Abouts == null)
+            {
+                return NotFound();
+            }
+
+            var about = await _context.Abouts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+
+            return View(about);
+        }
+
+        // GET: Abouts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Abouts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AboutMe")] About about)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(about);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(about);
+        }
+
+        // GET: Abouts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Abouts == null)
+            {
+                return NotFound();
+            }
+
+            var about = await _context.Abouts.FindAsync(id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+            return View(about);
+        }
+
+        // POST: Abouts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AboutMe")] About about)
+        {
+            if (id != about.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(about);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AboutExists(about.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(about);
+        }
+
+        // GET: Abouts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Abouts == null)
+            {
+                return NotFound();
+            }
+
+            var about = await _context.Abouts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (about == null)
+            {
+                return NotFound();
+            }
+
+            return View(about);
+        }
+
+        // POST: Abouts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Abouts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Abouts'  is null.");
+            }
+            var about = await _context.Abouts.FindAsync(id);
+            if (about != null)
+            {
+                _context.Abouts.Remove(about);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AboutExists(int id)
+        {
+            return _context.Abouts.Any(e => e.Id == id);
+        }
+    }
+}
